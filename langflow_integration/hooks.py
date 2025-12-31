210d01d6398bd016072c4a4ce3b90ca65fbbdc99
yazan01 <119818390+yazan01@users.noreply.github.com>
2025-12-31 12:15:53 +0300

    Update hooks.py

--- langflow_integration/hooks.py
+++ langflow_integration/hooks.py
@@ -27,6 +27,7 @@ app_license = "mit"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/langflow_integration/css/langflow_integration.css"
 # app_include_js = "/assets/langflow_integration/js/langflow_integration.js"
+app_include_js = "/assets/langflow_integration/js/langflow_global.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/langflow_integration/css/langflow_integration.css"
